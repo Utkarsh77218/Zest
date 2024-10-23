2eedd8c4083040f94a0fbc4703df3126367d7798
Utkarsh77218 <saxena2002utkarsh@gmail.com>
2024-10-23 23:38:12 +0530

    More Changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { useRouter } from "next/navigation";
 
 import { useCurrent } from "@/features/auth/api/use-current";
 import { useLogout } from "@/features/auth/api/use-logout";
-import { Button } from "@/components/ui/button";
+import { UserButton } from "@/features/auth/components/user-button";
 
 export default function Home() {
   const router = useRouter();
@@ -20,10 +20,7 @@ export default function Home() {
 
   return (
     <div>
-      Visible only to authorized users
-      <Button onClick={() => mutate()}>
-        Logout
-      </Button>
+      <UserButton />
     </div>
   );
 }
