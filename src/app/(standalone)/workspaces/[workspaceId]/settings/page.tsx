43164fc4bb4238048bda47bcd6a6288688cc35f0
Utkarsh77218 <saxena2002utkarsh@gmail.com>
2024-11-06 23:15:30 +0530

    ADDED more

--- src/app/(standalone)/workspaces/[workspaceId]/settings/page.tsx
+++ src/app/(standalone)/workspaces/[workspaceId]/settings/page.tsx
@@ -1,7 +1,8 @@
 import { redirect } from "next/navigation";
 
 import { getCurrent } from "@/features/auth/actions";
-import { parseParameter } from "next/dist/shared/lib/router/utils/route-regex";
+import { EditWorkspaceForm } from "@/features/workspaces/components/edit-workspace-form";
+import { getWorkspace } from "@/features/workspaces/actions";
 
 interface WorkspaceIdSettingsPageProps {
     params: {
@@ -13,9 +14,15 @@ const WorkspaceIdSettingsPage = async ({ params }: WorkspaceIdSettingsPageProps)
     const user = await getCurrent();
     if(!user)   redirect("/signin");
 
+    const initialValues = await getWorkspace({ workspaceId: params.workspaceId });
+
+    if(!initialValues) {
+        redirect(`/workspaces/${params.workspaceId}`);
+    }
+
     return (
         <div>
-            WorkspaceIdSettingsPage: {params.workspaceId}
+            <EditWorkspaceForm initialValues={initialValues} />
         </div>
     );
 };
