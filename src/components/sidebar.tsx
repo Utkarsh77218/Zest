4093944ee08afc6b81a48e03809d513f2c20067a
Utkarsh77218 <103490917+Utkarsh77218@users.noreply.github.com>
2025-03-29 00:40:38 +0530

    Update sidebar.tsx

--- src/components/sidebar.tsx
+++ src/components/sidebar.tsx
@@ -9,7 +9,7 @@ import { Projects } from "./projects";
 export const Sidebar = () => {
     return (
         <aside className="h-full bg-neutral-100 p-4 w-full">
-            <Link href="/" className='flex items-center p-3 rounded-md'>
+            <Link href="/" className='flex items-center rounded-md'>
                  <Image src="/logo.svg" alt="logo" height={46} width={48} /><p className='ml-2 font-bold text-xl'>Zest</p>
             </Link>
             <DottedSeparator className="my-4" />
